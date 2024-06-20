0e054f6dc1210d98da94ebebed35df56ed4cbb8d
ahmedraza0s <westernflexdomains@gmail.com>
2024-06-20 13:24:18 +0530

    ahmed

--- wf/frontend/src/components/navbar/navbar.jsx
+++ wf/frontend/src/components/navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './navbar.css';
 import cartIcon from '../assets/cart.png'; // Import the cart image
 
@@ -29,25 +30,24 @@ const Navbar = () => {
             [id === 'min-price' ? 'min' : 'max']: parseInt(value),
         });
     };
-    
 
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                <a href="/"><img src="logo.jpg" alt="Logo" /></a>
+                <Link to="/"><img src="logo.jpg" alt="Logo" /></Link>
             </div>
             <div className="search-bar">
                 <input type="text" placeholder="Search" />
                 <i className="fas fa-search"></i>
             </div>
             <ul className="navbar-nav">
-                <li><a href="#">Home</a></li>
+                <li><Link to="/">Home</Link></li>
                 <li className="dropdown">
-                    <a href="#" className="dropbtn">Shop <i className="fas fa-caret-down"></i></a>
+                    <Link to="/shop" className="dropbtn">Shop <i className="fas fa-caret-down"></i></Link>
                     <div className="dropdown-content">
                         <div className="has-sub">
                             <a href="#" onClick={togglePriceRange}>Filter by</a>
-                            <div id="price-range" className="price-range" style={{ display: priceRangeVisible ? 'block' : 'none' }}>
+                            <div id="price-range" className={`price-range ${priceRangeVisible ? 'visible' : ''}`}>
                                 <input type="range" min="499" max="999" step="1" value={price.min} id="price-slider" onInput={updatePriceRange} />
                                 <div className="range-values">
                                     <input type="number" id="min-price" value={price.min} min="499" max="999" step="1" onInput={updatePriceFromInput} />
@@ -57,31 +57,31 @@ const Navbar = () => {
                         </div>
                         <div className="has-sub">
                             <a href="#" onClick={toggleSubMenu}>Discount Percentage <i className="fas fa-caret-right"></i></a>
-                            <div className="sub-menu" style={{ display: subMenuVisible ? 'block' : 'none' }}>
+                            <div className={`sub-menu ${subMenuVisible ? 'visible' : ''}`}>
                                 <a href="#">10% off or more</a>
                                 <a href="#">20% off or more</a>
                                 <a href="#">30% off or more</a>
                                 <a href="#">50% off or more</a>
                             </div>
                         </div>
-                        <a href="#">Pay on Delivery</a>
-                        <a href="#">New Arrivals</a>
-                        <a href="#">Your Wishlist</a>
-                        <a href="#">Best Sellers</a>
+                        <Link to="#">Pay on Delivery</Link>
+                        <Link to="#">New Arrivals</Link>
+                        <Link to="#">Your Wishlist</Link>
+                        <Link to="#">Best Sellers</Link>
                     </div>
                 </li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact Us</a></li>
+                <li><Link to="#">About Us</Link></li>
+                <li><Link to="#">Contact Us</Link></li>
                 <li className="dropdown">
                     <a href="#" className="dropbtn">Hello, Sign in <i className="fas fa-caret-down"></i></a>
                     <div className="dropdown-content">
-                        <a href="#">Your Account</a>
-                        <a href="#">Your Orders</a>
-                        <a href="#">Sign Out</a>
+                        <Link to="#">Your Account</Link>
+                        <Link to="#">Your Orders</Link>
+                        <Link to="#">Sign Out</Link>
                     </div>
                 </li>
                 <li className="cart-icon">
-                    <a href="#"><img src={cartIcon} alt="Cart" /></a>
+                    <Link to="#"><img src={cartIcon} alt="Cart" /></Link>
                 </li>
             </ul>
         </nav>
