a50211d957fa5e4c473b90e2e1607abcf5dae0e2
ahmedraza0s <westernflexdomains@gmail.com>
2024-06-23 11:52:19 +0530

    new update by ahmed

--- wf/frontend/src/App.js
+++ wf/frontend/src/App.js
@@ -4,8 +4,10 @@ import Navbar from './components/navbar/Navbar'; // Ensure the path is correct a
 import './components/navbar/navbar.css';        // Ensure the path is correct and case-sensitive
 import Shop from './pages/Shop';                // Ensure the path is correct and case-sensitive
 import Home from './pages/Home';                // Ensure the path is correct and case-sensitive
+import Checkout from './pages/Checkout';
 import Login from './pages/Login';              // Ensure the path is correct and case-sensitive
 import Register from './pages/Register';
+
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
         <Route path="/shop" element={<Shop />} />    {/* Shop route */}
         <Route path="/login" element={<Login />} />  {/* Login route */}
         <Route path="/register" element={<Register />} /> {/* Register route */}
+        <Route path="/Checkout" element={<Checkout />} />
         {/* Add other routes here */}
       </Routes>
     </BrowserRouter>
